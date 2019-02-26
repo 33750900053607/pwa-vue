3ffe88563e04589c035bbf87037b05385317a0ab
33750900053607 <47789111+33750900053607@users.noreply.github.com>
2019-02-26 23:55:39 +0900

    manifest change
    start URL

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ee52dc5fe8c8a9ff07f8bbdc6b97a76.js"
+  "/precache-manifest.fd63736be9b3ea69a429bb241b660ac4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "easy-wallet"});
@@ -27,6 +27,19 @@ workbox.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "2694c49f042b15763d38e116fe3e4b4f"
+  },
+  {
+    "url": "precache-manifest.8ee52dc5fe8c8a9ff07f8bbdc6b97a76.js",
+    "revision": "8ee52dc5fe8c8a9ff07f8bbdc6b97a76"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "b9cc3adf53849f485fe4bc9a5f47276a"
+  }
+].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
